7300f1c439df392060d600587f1fc168f948c170
seohun <rentalcuedev@gmail.com>
2020-04-15 03:39:39 +0900

    .

--- resources/views/pub.blade.php
+++ resources/views/pub.blade.php
@@ -7,8 +7,29 @@
 
       <li><a href="/site/login">로그인</a></li>
       <li><a href="/site/register/step01">회원가입-step01</a></li>
+
+      <p> 본인인증 이후에.. 직접가입 OR SNS 가입 갈라치기 선택하도록.. </p>
+
+
       <li><a href="/site/register/step02">회원가입-step02</a></li>
+
+      <p> 프로필 이미지를 아예 빼버리고 회원 정보 관리에서 입력 하도록 할까.. (여기에 있으면 어려울수도.) </p>
+
       <li><a href="/site/register/step02-sns">회원가입(SNS)</a></li>
+
+      <li><a href="/site/auth">아이디/비밀번호 찾기</a></li>
+
+      <li><a href="/site/serach">검색(모바일)</a></li>
+
+      <li><a href="/site/serach">검색결과</a></li>
+
+      <li><a href="/item/recommend">뭐치지? (구분)</a></li>
+
+      <li><a href="/recommend/goodcue">추천</a></li>
+      <li><a href="/recommend/hot">인기 좋은 큐</a></li>
+      <li><a href="/recommend/review">평가 좋은 큐</a></a></li>
+      <li><a href="//recommend/signature">시그니처 큐</a></li>
+    
     </ol>
     <br />
     <br /><br /><br /><br /><br /><br />
